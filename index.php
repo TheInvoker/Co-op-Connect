908255af190f3b3ce160cf9b287ac9f818eeca66
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-02-17 20:33:15 -0500

    added about page

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 		<?php
 			include "snippet/head.php";
 		?>
-		
+
 		<!-- Include the Google maps library -->
 		<script src="http://maps.googleapis.com/maps/api/js?sensor=false&amp;libraries=places"></script>
 		<!-- Include the jquery maps library -->
@@ -13,7 +13,7 @@
 		<script src="js/markerclusterer_packed.js"></script>
 		<!-- Include the geolocation autocomplete library -->
 		<script src="js/jquery.geocomplete.min.js"></script>
-		
+
 		<script src="js/xdate.js"></script>
 		<script src="js/xdate.i18n.js"></script>
 		<script src="js/mobipick.js"></script>
@@ -29,7 +29,7 @@
 		<script src="js/custom/resource.js"></script>
 		<script src="js/custom/message.js"></script>
 		<script src="js/custom/about.js"></script>
-		
+
 		<link rel="stylesheet" type="text/css" href="css/mobipick.css">
 	</head>
 	<body>
@@ -47,15 +47,15 @@
 					<input name="password" type="password" autocomplete="off" maxlength="255" value="test" required>
 					<input type="submit" value="Login">
 				</form>
-				
+
 				<div align="center">
 					<a id="register-button" href="#" class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-arrow-u ui-btn-icon-left">Register</a>
 					<a id="forgot-button" href="#" class="ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-arrow-d ui-btn-icon-left">Forgot Password</a>
-				</div>				
+				</div>
 			</div>
-		</div> 
-		
-		
+		</div>
+
+
 		<div id="register-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Register</h1>
@@ -63,7 +63,7 @@
 
 			<div data-role="main" class="ui-content">
 				<form id="register-form">
-				
+
 					<label for="firstname">First Name:</label>
 					<input name="firstname" type="text" maxlength="255" required>
 
@@ -72,50 +72,50 @@
 
 					<label for="email">Email Address:</label>
 					<input name="email" type="email" maxlength="255" required>
-					
+
 					<label for="password">Password:</label>
 					<input name="password" type="password" maxlength="255" required>
-					
+
 					<label for="c-password">Confirm Password:</label>
 					<input name="c-password" type="password" maxlength="255" required>
-					
+
 					<input type="submit" value="Register">
 				</form>
 			</div>
-		</div> 
-		
-		
-		
-		
-		
+		</div>
+
+
+
+
+
 		<div id="menu-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Main Menu</h1>
 			</div>
 
 			<div data-role="main" class="ui-content">
-				<a id="profile-button" href="#" data-role="button" data-icon="user" data-iconpos="left">My Profile</a> 
-				<a id="placement-button" href="#" data-role="button" data-icon="shop" data-iconpos="left">My Placements</a> 
-				<a id="search-button" href="#" data-role="button" data-icon="search" data-iconpos="left">Search</a> 
-				<a id="map-button" href="#" data-role="button" data-icon="location" data-iconpos="left">Map</a> 
+				<a id="profile-button" href="#" data-role="button" data-icon="user" data-iconpos="left">My Profile</a>
+				<a id="placement-button" href="#" data-role="button" data-icon="shop" data-iconpos="left">My Placements</a>
+				<a id="search-button" href="#" data-role="button" data-icon="search" data-iconpos="left">Search</a>
+				<a id="map-button" href="#" data-role="button" data-icon="location" data-iconpos="left">Map</a>
 				<a id="message-button" href="#" data-role="button" data-icon="comment" data-iconpos="left">
 					Messages
 					<span id="message-number" class="ui-li-count ui-btn-corner-all countBubl" style="display:none;"></span>
-				</a> 
+				</a>
 				<a id="resource-button" href="#" data-role="button" data-icon="alert" data-iconpos="left">
 					Resources
 					<span id="resource-number" class="ui-li-count ui-btn-corner-all countBubl" style="display:none;"></span>
 				</a>
-				<a id="about-button" href="#" data-role="button" data-icon="info" data-iconpos="left">About</a> 
+				<a id="about-button" href="#" data-role="button" data-icon="info" data-iconpos="left">About</a>
 			</div>
-		</div> 
-		
-		
-		
-		
-		
-		
-		
+		</div>
+
+
+
+
+
+
+
 
 		<div id="profile-page" data-role="page" data-theme="b">
 			<div data-role="header">
@@ -125,7 +125,7 @@
 			<div data-role="main" class="ui-content">
 				<h2 id="profile-fullname"></h2>
 				<img src="" id="profile-avatar-image" class="avatar-image"/>
-				
+
 				<div class="ui-field-contain">
 					<fieldset data-role="controlgroup" data-type="horizontal">
 						<a id="profile-email" class="ui-shadow ui-btn ui-corner-all ui-icon-mail ui-btn-icon-left">Email</a>
@@ -135,14 +135,14 @@
 						<a id="profile-placements" class="ui-shadow ui-btn ui-corner-all ui-icon-shop ui-btn-icon-left">Placements</a>
 					</fieldset>
 				</div>
-				
+
 				<p id="profile-status"></p>
 				<p id="profile-biotext"></p>
 				<p id="profile-info"></p>
-				
-				<a id="profile-edit-button" href="#" data-role="button" data-icon="edit" data-iconpos="left">Edit</a> 
+
+				<a id="profile-edit-button" href="#" data-role="button" data-icon="edit" data-iconpos="left">Edit</a>
 			</div>
-		</div> 
+		</div>
 		<div id="profile-edit-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Profile Edit</h1>
@@ -150,10 +150,10 @@
 
 			<div data-role="main" class="ui-content">
 				<form id="profile-edit-form">
-				
+
 					<label for="file">Avatar Image:</label>
 					<input name="file" type="file" accept="image/*">
-				
+
 					<label for="firstname">First Name:</label>
 					<input name="firstname" type="text" maxlength="255" required>
 
@@ -162,22 +162,22 @@
 
 					<label for="email">Email Address:</label>
 					<input name="email" type="email" maxlength="255" required>
-					
+
 					<label for="phone">Phone Number:</label>
 					<input name="phone" type="tel" maxlength="255">
-					
+
 					<label for="website">Website:</label>
 					<input name="website" type="url" maxlength="255">
-					
+
 					<label for="status">Status:</label>
 					<textarea name="status" cols="40" rows="2" maxlength="16777215"></textarea>
-					
+
 					<label for="biotext">Bio Text:</label>
 					<textarea name="biotext" cols="40" rows="2" maxlength="4294967295"></textarea>
-					
+
 					<fieldset id="profile_edit_department_rb" data-role="controlgroup" data-type="horizontal" data-mini="true">
 						<legend>Co-op Department:</legend>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "department";
@@ -186,17 +186,17 @@
 						?>
 
 					</fieldset>
-					
+
 					<input type="submit" value="Save">
 				</form>
 			</div>
-		</div> 
-		
-		
-		
-		
-		
-		
+		</div>
+
+
+
+
+
+
 
 		<div id="placement-page" data-role="page" data-theme="b">
 			<div data-role="header">
@@ -208,9 +208,9 @@
 
 				</ul>
 
-				<a id="add-placement-button" href="#" data-role="button" data-icon="plus" data-iconpos="left">Add</a> 
+				<a id="add-placement-button" href="#" data-role="button" data-icon="plus" data-iconpos="left">Add</a>
 			</div>
-			
+
 			<div id="placementMenu" data-role="popup" data-theme="b">
 				<ul data-role="listview" data-inset="true" style="min-width:210px;">
 					<li data-role="list-divider">Menu</li>
@@ -220,7 +220,7 @@
 					<li><a href="#" class="ui-btn ui-icon-delete ui-btn-icon-left">Delete</a></li>
 				</ul>
 			</div>
-		</div> 
+		</div>
 		<div id="placement-edit-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Placement Edit</h1>
@@ -228,30 +228,30 @@
 
 			<div data-role="main" class="ui-content">
 				<form id="placement-edit-form">
-				
+
 					<label for="address">Address:</label>
 					<input name="address" type="text" maxlength="255" required>
-					
+
 					<img src="images/site/powered-by-google-on-non-white.png" align="right"/>
-				
+
 					<input name="name" type="hidden" maxlength="255">
 					<input name="locality" type="hidden" maxlength="255">
 					<input name="country" type="hidden" maxlength="255">
 					<input name="lat" type="hidden" maxlength="255">
 					<input name="lng" type="hidden" maxlength="255">
-					
+
 					<label for="role">Role:</label>
 					<input name="role" type="text" maxlength="255" required>
-					
+
 					<label for="company">Company:</label>
 					<input name="company" type="text" maxlength="255" required>
-					
+
 					<label for="date_start">Start Date:</label>
 					<input name="date_start" type="date" required/>
-					
+
 					<label for="date_end">End Date:</label>
 					<input name="date_end" type="date" required/>
-					
+
 					<label for="active">Active:</label>
 					<select name="active" data-role="slider">
 						<option value="0">No</option>
@@ -261,9 +261,9 @@
 					<input type="submit" value="Save">
 				</form>
 			</div>
-		</div> 
-		
-		
+		</div>
+
+
 		<div id="checklist-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Checklist</h1>
@@ -271,12 +271,12 @@
 
 			<div data-role="main" class="ui-content">
 				<fieldset id="checklistCB" data-role="controlgroup">
-				</fieldset>		
+				</fieldset>
 			</div>
-		</div> 
+		</div>
 
-		
-		
+
+
 		<div id="search-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Search</h1>
@@ -284,13 +284,13 @@
 
 			<div data-role="main" class="ui-content">
 				<form id="search-form">
-				
+
 					<input name="search" type="search" maxlength="255" data-mini="true" required>
 
 					<table width="100%">
 						<tr>
 							<td width="50%">
-								<a id="search-setting-button" href="#" data-role="button" data-icon="gear" data-iconpos="left" data-mini="true">Settings</a> 
+								<a id="search-setting-button" href="#" data-role="button" data-icon="gear" data-iconpos="left" data-mini="true">Settings</a>
 							</td>
 							<td width="50%">
 								<input type="submit" value="Search" data-mini="true">
@@ -298,7 +298,7 @@
 						</tr>
 					</table>
 				</form>
-				
+
 				<table data-role="table" id="search-table" data-mode="columntoggle" class="ui-body-d ui-shadow table-stripe ui-responsive" data-column-btn-theme="b" data-column-btn-text="Columns to display..." data-column-popup-theme="b">
 					<thead>
 						<tr class="ui-bar-d">
@@ -313,7 +313,7 @@
 					</tbody>
 				</table>
 			</div>
-		</div> 
+		</div>
 		<div id="search-settings-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Settings</h1>
@@ -322,7 +322,7 @@
 			<div data-role="main" class="ui-content">
 				<form id="search-settings-form">
 					<h4>User Filters:</h4>
-				
+
 					<label for="firstname">First Name:</label>
 					<input name="firstname" type="text" maxlength="255">
 
@@ -331,12 +331,12 @@
 
 					<label for="email">Email Address:</label>
 					<input name="email" type="text" maxlength="255">
-					
+
 					<fieldset id="search_department_cb" data-role="controlgroup" data-mini="true">
 						<legend>Co-op Department:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "department";
@@ -345,12 +345,12 @@
 						?>
 
 					</fieldset>
-					
+
 					<fieldset id="search_role_cb" data-role="controlgroup" data-mini="true">
 						<legend>Role:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "role";
@@ -359,14 +359,14 @@
 						?>
 
 					</fieldset>
-					
+
 					<h4>Placement Filters:</h4>
-					
+
 					<fieldset id="search_city_cb" data-role="controlgroup" data-mini="true">
 						<legend>City:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "city";
@@ -375,12 +375,12 @@
 						?>
 
 					</fieldset>
-					
+
 					<fieldset id="search_country_cb" data-role="controlgroup" data-mini="true">
 						<legend>Country:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "country";
@@ -389,12 +389,12 @@
 						?>
 
 					</fieldset>
-					
+
 					<fieldset id="search_topic_cb" data-role="controlgroup" data-mini="true">
 						<legend>Role:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "topic";
@@ -403,12 +403,12 @@
 						?>
 
 					</fieldset>
-					
+
 					<fieldset id="search_company_cb" data-role="controlgroup" data-mini="true">
 						<legend>Company:</legend>
 						<a href="#" class="clear-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 						<a href="#" class="selectall-cb-button ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Select All</a>
-						
+
 						<?php
 							$_GET['page'] = "html/printfieldhtml";
 							$_GET['field'] = "company";
@@ -417,27 +417,27 @@
 						?>
 
 					</fieldset>
-					
+
 					<label for="date_start">Start Date:</label>
 					<input name="date_start" type="date"/>
 					<a href="#" class="clear-text-date ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 					<a href="#" class="set-text-today ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Today</a>
-					
+
 					<label for="date_end">End Date:</label>
 					<input name="date_end" type="date"/>
 					<a href="#" class="clear-text-date ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-minus ui-btn-icon-left">Clear</a>
 					<a href="#" class="set-text-today ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-b ui-mini ui-icon-plus ui-btn-icon-left">Today</a>
-					
+
 					<input name="active" type="hidden" maxlength="255" value="1">
 				</form>
 			</div>
-		</div> 
-		
-		
-		
-		
-		
-		<div id="map-page" data-role="page" data-theme="b">			
+		</div>
+
+
+
+
+
+		<div id="map-page" data-role="page" data-theme="b">
 			<div role="main" class="ui-content" id="map_canvas">
 				<!-- map loads here... -->
 			</div>
@@ -450,7 +450,7 @@
 				<input type="submit" value="Update">
 			</form>
 		</div>
-		
+
 		<div id="thread-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Threads</h1>
@@ -461,26 +461,26 @@
 
 				</ul>
 			</div>
-		</div> 
+		</div>
 		<div id="message-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Messages</h1>
 			</div>
 
 			<div data-role="main" class="ui-content">
-				<a id="more-message-button" href="#" data-role="button" data-icon="arrow-d" data-iconpos="left">Show More</a> 
-				
+				<a id="more-message-button" href="#" data-role="button" data-icon="arrow-d" data-iconpos="left">Show More</a>
+
 				<div id="message-list">
 				</div>
-				
+
 				<form id="message-form">
 					<input name="message" type="text" maxlength="255" required/>
 					<input type="submit" value="Send">
 				</form>
 			</div>
-		</div> 
-		
-		
+		</div>
+
+
 		<div id="resource-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>Resources</h1>
@@ -491,17 +491,55 @@
 
 				</ul>
 			</div>
-		</div> 
-		
+		</div>
+
 		<div id="about-page" data-role="page" data-theme="b">
 			<div data-role="header">
 				<h1>About</h1>
 			</div>
 
 			<div data-role="main" class="ui-content">
+				<section>
+					<header>
+						<h1>Co-op Connect</h1>v1.0.0
+					</header>
+				</section>
 				
+				<section>
+					<header>
+						<h4>BackStory:</h4>
+					</header>
+					<p>
+						This app was originally developed in Android for the <a href="http://blog.utsc.utoronto.ca/appstar" target="_blank">AppStar</a> contest at <a href="http://blog.utsc.utoronto.ca/thehub" target="_blank">TheHub</a> in 2013 for the <a href="http://www.utsc.utoronto.ca/admissions/programs/international-development-studies" target="_blank">IDS</a> department. This app won first place in the education category. To see details, click <a href="http://blog.utsc.utoronto.ca/thehub/2013/10/13/appstar-2013-1st-prize-education-goes-to-ids-connect" target="_blank">here</a> and <a href="http://ose.utsc.utoronto.ca/ose/story.php?id=5422" target="_blank">here</a>. In early 2015, it was ported to a web app optimized for mobile devices.
+					</p>
+				</section>
+				
+				<section>
+					<header>
+						<h4>Developers:</h4>
+					</header>
+					<ol>
+						<li><a href="http://www.linkedin.com/profile/view?id=282676120" target="_blank">Ryan D'Souza</a></li>
+					</ol>
+				</section>
+				
+				<section>
+					<header>
+						<h4>Credits:</h4>
+					</header>
+					<ol>
+						<li><a href="http://jquery.com" target="_blank">jQuery</a></li>
+						<li><a href="http://jquerymobile.com" target="_blank">jQuery Mobile</a></li>
+						<li><a href="https://github.com/gregjacobs/Autolinker.js" target="_blank">Autolinker.js</a></li>
+						<li><a href="https://developers.google.com/maps/documentation/javascript" target="_blank">Google Maps</a></li>
+						<li><a href="http://ubilabs.github.io/geocomplete" target="_blank">Geocomplete</a></li>
+						<li><a href="https://code.google.com/p/jquery-ui-map" target="_blank">jquery-ui-map</a></li>
+						<li><a href="https://code.google.com/p/google-maps-utility-library-v3/wiki/Libraries" target="_blank">google-maps-utility-library-v3</a></li>
+						<li><a href="http://mobipick.sustainablepace.net" target="_blank">Mobi Pick</a></li>
+					</ol>
+				</section>
 			</div>
-		</div> 
-		
+		</div>
+
 	</body>
 </html>
