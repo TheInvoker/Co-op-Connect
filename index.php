69c0c325a2e35d6d6a77302baa2dad497e3d4320
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-03-14 20:06:03 -0400

    fix

--- index.php
+++ index.php
@@ -307,7 +307,7 @@
 				
 				<br/>
 				
-				<table id="search-table" class="footable" data-page-navigation=".pagination" data-page-size="20">
+				<table id="search-table" class="footable" data-page-navigation=".pagination" data-page-size="2">
 					<thead>
 						<tr>
 							<th data-type="numeric">#</th>
