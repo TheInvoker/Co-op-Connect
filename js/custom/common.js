04167a4cd4299ad9a0583efd8e315420a19f734d
Ryan D'Souza <ryan.dsouza@hotmail.ca>
2015-03-15 18:40:42 -0400

    fix

--- js/custom/common.js
+++ js/custom/common.js
@@ -258,7 +258,7 @@ function swipePanel(pageId, leftPanelId) {
 			// We check if there is no open panel on the page because otherwise
 			// a swipe to close the left panel would also open the right panel (and v.v.).
 			// We do this by checking the data that the framework stores on the page element (panel: open).
-			if (x < 50) {
+			if (x <= 50) {
 				if ( $.mobile.activePage.jqmData( "panel" ) !== "open" ) {
 					if ( e.type === "swiperight" ) {
 						$( leftPanelId ).panel( "open" );
